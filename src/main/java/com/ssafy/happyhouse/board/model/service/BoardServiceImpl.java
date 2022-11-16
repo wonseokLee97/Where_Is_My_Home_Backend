3745a7272bc56d5eb86448af2d674842db703752
sbsc20 <sbsc20@naver.com>
2022-11-16 10:58:34 +0900

    board

--- src/main/java/com/ssafy/happyhouse/board/model/service/BoardServiceImpl.java
+++ src/main/java/com/ssafy/happyhouse/board/model/service/BoardServiceImpl.java
@@ -2,7 +2,6 @@ package com.ssafy.happyhouse.board.model.service;
 
 import java.util.List;
 
-import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -71,7 +70,6 @@ public class BoardServiceImpl implements BoardService {
 	@Override
 	@Transactional
 	public boolean deleteArticle(int articleno) throws Exception {
-		boardMapper.deleteArticle(articleno);
 		return boardMapper.deleteArticle(articleno) == 1;
 	}
 
